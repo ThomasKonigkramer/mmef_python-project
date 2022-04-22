50651ada6835805ebe2e82f05dd35bd85ddb6f07
Sukanya Mukherjee <87748861+GeetX@users.noreply.github.com>
2022-04-22 09:43:50 +0200

    1. Test added

--- Price.py
+++ Price.py
@@ -5,11 +5,18 @@ author: Sukanya Mukherjee
 
 Pricing Model - we use this to present the three shipping method price options and to calculate the pre-tax and discount price.
 """
+
+import pandas
+
+#from tabulate import tabulate
+
+
 #import order
 
+
 distance_code = 2 #range circles 1 to 5
-shipping_method = 1.5 #Priority(1.5) /Express(1) /Standard (0)"
-shipping_destination = 1 #"Domestic- EU(0)/International(1)"
+shipping_method = "Priority" #Priority(1.5) /Express(1) /Standard (0)"
+shipping_destination = 1 #"Domestic/EU/International(1)"
 package_size = 1.5 #Small 0/ Medium 1/ Big 1.5"
 fragile = 1 #is it fragile?
 
@@ -27,14 +34,19 @@ class Price:
     def get_price(self):
          if self.shipping_method == "Priority":
             price = baseprice + 5*shipping_destination + 2.5*package_size + 2.5*distance_code + 5
+            return price
          elif self.shipping_method == "Express":
             price =  baseprice + 5*shipping_destination + 2.5*package_size + 2.5*distance_code + 2.5
+            return price
          elif self.shipping_method == "Standard":
-            price = price = baseprice + 5*shipping_destination + 2.5*package_size + 2.5*distance_code
+            price = baseprice + 5*shipping_destination + 2.5*package_size + 2.5*distance_code
+            return price
 
     def get_price_options(self):
-        from tabulate import tabulate
-        print(tabulate([['Priority', '24 hours',24], ['Express','2-3 days', 19],['Standard','5-6 days', 19]], headers=['Service', 'Expected time', 'Price']))
+        print("Your price options are :", tabulate([['Priority', '24 hours',24], ['Express','2-3 days', 19],['Standard','5-6 days', 19]], headers=['Service', 'Expected time', 'Price']))
 
 
-user = Price("")
+"""test"""
+
+user = Price("Standard", 1, 1.5, 2)
+print('Your recommended price is :', user.get_price(), "Euros")
