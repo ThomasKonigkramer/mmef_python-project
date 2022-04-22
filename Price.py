9c5301ccd13b86abe44165c23e7be443042e4a0e
Sukanya Mukherjee <87748861+GeetX@users.noreply.github.com>
2022-04-22 23:22:40 +0200

    commit with classes

--- Price.py
+++ Price.py
@@ -6,47 +6,60 @@ author: Sukanya Mukherjee
 Pricing Model - we use this to present the three shipping method price options and to calculate the pre-tax and discount price.
 """
 
-import pandas
-
-#from tabulate import tabulate
-
-
-#import order
+from tabulate import tabulate
+from enum import Enum
 
 
-distance_code = 2 #range circles 1 to 5
-shipping_method = "Priority" #Priority(1.5) /Express(1) /Standard (0)"
-shipping_destination = 1 #"Domestic/EU/International(1)"
-package_size = 1.5 #Small 0/ Medium 1/ Big 1.5"
-fragile = 1 #is it fragile?
+class ShippingDetail:
 
-baseprice = 8
+    shipping_method = ''
+    shipping_time = ''
+    premium = 0.0
+
+    def __init__(self,  shipping_method, shipping_time, premium):
+        self.shipping_method = shipping_method
+        self.shipping_time = shipping_time
+        self.premium = premium
+
+
+priority = ShippingDetail('Priority', '1 day', 5)
+express = ShippingDetail('Express', '4 hours', 2.5)
+standard = ShippingDetail('Standard', '2-3 days',0)
+
+
+baseprice = 8.0
 
 """price pre VAT/discounts"""
 
 class Price:
-    def __init__(self, shipping_method, shipping_destination,package_size,distance_code):
+
+    def __init__(self, shipping_method, shipping_destination, package_size, distance_code):
         self.shipping_method = shipping_method
         self.shipping_destination = shipping_destination
         self.package_size = package_size
         self.distance_code = distance_code
 
-    def get_price(self):
-         if self.shipping_method == "Priority":
-            price = baseprice + 5*shipping_destination + 2.5*package_size + 2.5*distance_code + 5
+    def get_price(self, shipping_meth):
+         if shipping_meth == priority.shipping_method:
+            price = baseprice + 5*self.shipping_destination + 2.5*self.package_size + 2.5*self.distance_code + priority.premium
             return price
-         elif self.shipping_method == "Express":
-            price =  baseprice + 5*shipping_destination + 2.5*package_size + 2.5*distance_code + 2.5
+         elif shipping_meth == express.shipping_method:
+            price =  baseprice + 5*self.shipping_destination + 2.5*self.package_size + 2.5*self.distance_code + express.premium
             return price
-         elif self.shipping_method == "Standard":
-            price = baseprice + 5*shipping_destination + 2.5*package_size + 2.5*distance_code
+         elif shipping_meth == standard.shipping_method:
+            price = baseprice + 5*self.shipping_destination + 2.5*self.package_size + 2.5*self.distance_code
             return price
+
 
     def get_price_options(self):
-        print("Your price options are :", tabulate([['Priority', '24 hours',24], ['Express','2-3 days', 19],['Standard','5-6 days', 19]], headers=['Service', 'Expected time', 'Price']))
-
+        print("Your price options are :")
+        print('')
+        print(tabulate([['Priority', '1 day', self.get_price(priority.shipping_method)], ['Express', '2-3 days', self.get_price(express.shipping_method)], ['Standard', '5-6 days', 19]], headers=['Service', 'Expected time', 'Price(€)']))
 
 """test"""
 
-user = Price("Standard", 1, 1.5, 2)
-print('Your recommended price is :', user.get_price(), "Euros")
+user = Price("Express", 1, 1.5, 2)
+
+print('\nYour recommended price is :', user.get_price(user.shipping_method), "€")
+
+user.get_price_options()
