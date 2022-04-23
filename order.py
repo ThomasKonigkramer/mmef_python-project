d36a4786c6bec6b4117c4d09960544e248928cba
Toufu0928 <102315031+Toufu0928@users.noreply.github.com>
2022-04-23 22:35:01 +0200

    Update order.py

--- order.py
+++ order.py
@@ -1,8 +1,8 @@
-#title : python project - KMX Shipping Platform, new order class
-#    author : Xiao JIANG
-## place a new order ### only focus on delivery from france
-# Demand analysis to get the price throuth the address and size
-#first to get the distance of the delivery
+# #title : python project - KMX Shipping Platform, new order class
+# #    author : Xiao JIANG
+# ## place a new order ### only focus on delivery from france
+# # Demand analysis to get the price throuth the address and size
+# #first to get the distance of the delivery
 
 print('where are you shipping from:')
 class From:
@@ -33,7 +33,8 @@ class From:
     def get_country(self):
         return self.country
     country = input('country:')
-    if country == 'france':
+    country = str.upper(country)
+    if country == 'FRANCE':
         print(country)
     else:
         print('we do not suopport the service yet')
@@ -80,28 +81,29 @@ class Destination:
         phonenumber = input('please enter the correct phonenumber:')
     if len(phonenumber) == 10:
         print(phonenumber)
-    def get_country(self):
-        return self.country
-zone1 = ['FRANCE']
-zone2 = ['AUSTRIA, BELGIUM, BULGARIA, CROATIA, REPUBLIC OF CYPRUS, CZECH REPUBLIC, DENMARK, ESTONIA, FINLAND, GERMANY, GREECE, HUNGARY, IRELAND, ITALY, LATVIA, LITHUANIA, LUXEMBOURG, MALTA, NETHERLANDS, POLAND, PORTUGAL, ROMANIA, SLOVAKIA, SLOVENIA, SPAIN, SWEDEN']
-country = input('country:')
-country = str.upper(country)
-if country in zone1:
-    print('Domestic')
-elif country in zone2:
-    print('European')
-else:
-    print('International')
 
-    def get_city(self):
-        return self.city
+def get_country(country):
+    zone1 = ['FRANCE']
+    zone2 = ['AUSTRIA, BELGIUM, BULGARIA, CROATIA, REPUBLIC OF CYPRUS, CZECH REPUBLIC, DENMARK, ESTONIA, FINLAND, GERMANY, GREECE, HUNGARY, IRELAND, ITALY, LATVIA, LITHUANIA, LUXEMBOURG, MALTA, NETHERLANDS, POLAND, PORTUGAL, ROMANIA, SLOVAKIA, SLOVENIA, SPAIN, SWEDEN']
+    country = str.upper(country)
+    if country in zone1:
+        return 'Domestic'
+    elif country in zone2:
+        return 'European'
+    else:
+        return 'International'
+country = input('country:')
+print(get_country(country))
+
+
+def get_city(self):
+    return self.city
 
     city = input('city:')
     print(city)
 
-    def get_address(self, address):
-        return self.address
-
+def get_address(self, address):
+    return self.address
     address = input('address:')
     print(address)
 
@@ -112,7 +114,7 @@ else:
     print(code)
 
 class Package:
-    def __init__(self, weight, service):
+    def __init__(self, weight, shippingmethod):
         self.weight = weight
         self.service = service
 
@@ -120,27 +122,27 @@ class Package:
         return f'the weight is {self.weight}, the service is {self.service}'
 
     def get_weight(self):
-        return self.weight
+        if weight <= 1:
+            return ('small')
+        elif weight > 1 and weight <= 3:
+            return ('medium')
+        elif weight >= 4:
+            return ('big')
+        elif weight >= 10:
+            return ('it is too big to delivery')
     weight = input('weight:')
     weight = int(weight)
-    if weight <= 1:
-        print('small')
-    elif weight > 1 and weight <= 3:
-        print('medium')
-    elif weight >= 4:
-        print('big')
-def get_service(self):
-    return self.service
-service = input('please choose( 1:Priority 2:Express 3:Standard):')
-service = int(service)
-if service == 1:
-    print('price=')
-elif service == 2:
-    print('price=')
-else:
-    service == 3
-    print('price=')
-
+def get_shippingmethod(self):
+    if shippingmethod == 1:
+        return 'Priority'
+    elif shippingmethod == 2:
+        return 'Express'
+    else:
+        shippingmethod == 3
+        return 'Standard'
+shippingmethod = input('please choose( 1:Priority 2:Express 3:Standard):')
+shippingmethod = int(shippingmethod)
+#
 
 
 
