0e15a68df74fac6af01490cde54f2422473729b0
Sukanya Mukherjee <87748861+GeetX@users.noreply.github.com>
2022-04-27 11:43:34 +0200

    commit

--- Discount.py
+++ Discount.py
@@ -3,36 +3,47 @@ title : python project KMX Shipping Platform, Discounting
 
 author: Sukanya Mukherjee
 
-Discounts and Taxes
+Discounts
 """
 
 import Price
-class Discount:
-    def __init__(self, discount, type, validity, discountedamount):
+
+class Discount(Price.Price):
+    def __init__(self, price, discount, type, discountedamount, discountedprice):
+        
+        super().__init__(price)
+
         self.discount = discount
         self.type = type
-        self.validity = validity
-        self._discountedamount = discountedamount
+        self.discountedamount = discountedamount
+        self.discountedprice = discountedprice
+        
+        
 
-        self._discountedamount = 0.0
-
-    def set_disc(self)
-       if Price.price < 20:
-       return ("Minimum purchase of 20€ required to avail discount"
-       else
-
-
-discountedprice = .set_price()
-
+    def set_discounting(self):
+        if self.price < 20:
+           return ("Minimum purchase of 20€ required to avail discount") 
+        else : 
+            if self.discount == True and self.type == "%":
+                if self.discountedamount <10 :
+                    self.discountedprice = self.discountedprice*(1 - self.discountedamount)
+                else :
+                    return("Sorry! That is an invalid discount")
+            elif self.discount == True and self.type =="flat":
+                if self.discountedamount <5 :
+                    self.discountedprice = self.discountedprice - self.discountedamount
+                else :
+                    return("Sorry! That is an invalid discount")   
+            else :
+                self.discountedprice = self.price
+        return("Congrats! Your new price is", self.discountedprice,". You have saved", self.price - self.discountedprice,".")
+        
 """import from user menu"""
-user = Discount(True, "%","valid",5)
 
-if user.discount == True:
-    if user.type == "%":
-        discountedprice = discountedprice*(1 - user.discountedamount)
-elif user.type =="flat"
-        discountedprice = discountedprice - user.discountedamount
-elif
-    discountedprice = price
+user = Discount(5,True, "%",5,5)
+user.discountedprice
+
+
+
 
 
