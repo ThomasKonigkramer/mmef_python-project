b2bc774dbab66052d3a0b86bf75f6a7710177430
Sukanya Mukherjee <87748861+GeetX@users.noreply.github.com>
2022-04-28 15:55:50 +0200

    updated non discount

--- Discount.py
+++ Discount.py
@@ -6,6 +6,7 @@ author: Sukanya Mukherjee
 Discounts
 """
 
+from traceback import print_last
 from Price import Price
 
 class Discount(Price):
@@ -22,25 +23,32 @@ class Discount(Price):
         
     def set_discounting(self):
         if self.set_price(self._shipping_method) < 20:
-           return ("Minimum purchase of 20€ required to avail discount") 
-        else : 
-            if self.discount == True and self.type == "%":
+            self.discountedprice = self.set_price(self._shipping_method)
+        elif self.discount == True : 
+            if self.type == "%":
                 if self.discountamount <10 :
                     self.discountedprice = self.set_price(self._shipping_method)*(1 - self.discountamount/100)
                 else :
-                    return("Sorry! That is an invalid discount")
-            elif self.discount == True and self.type =="flat":
+                    return("Sorry! That is an invalid discount!")
+            elif self.type =="flat":
                 if self.discountamount <5 :
                     self.discountedprice = self.set_price(self._shipping_method) - self.discountamount
                 else :
-                    return("Sorry! That is an invalid discount")   
-            else :
-                self.discountedprice = self.set_price(self._shipping_method)
-        return(f'Congrats! Your new price is {self.discountedprice:.2f}. You have saved {self.price - self.discountedprice:.2f}')
+                    return("Sorry! That is an invalid discount!")   
+            return(f'Congrats! Your new price is {self.discountedprice:.2f}. You have saved {self.price - self.discountedprice:.2f}')
+        else :
+            self.discountedprice = self.set_price(self._shipping_method) 
+            return(f'Your price is {self.discountedprice:.2f}. You have not chosen a discount!')  
+        
+        
+            
+  
+    def final_price(self):
+        self.price = self.set_discounting()
         
 """import from user menu"""
 
-user = Discount(True, "%",5,5,'Priority',2,2,0,0)
+user = Discount(False, "%",5,5,'Priority',2,2,0,0)
 print(user.set_discounting())
 
 
